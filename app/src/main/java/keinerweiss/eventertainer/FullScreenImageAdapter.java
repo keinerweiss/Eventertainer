934fcab057a8e5ba64f104c6473ea5cd49ed0003
Rüdiger Marwein <henryxiv@hotmail.com>
2016-09-04 23:33:38 +0200

    Added gallery with AsyncTask

--- app/src/main/java/keinerweiss/eventertainer/FullScreenImageAdapter.java
+++ app/src/main/java/keinerweiss/eventertainer/FullScreenImageAdapter.java
@@ -2,8 +2,10 @@ package keinerweiss.eventertainer;
 
 import android.app.Activity;
 import android.content.Context;
+import android.database.DataSetObserver;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
@@ -13,6 +15,11 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.ArrayList;
 
 /**
@@ -20,10 +27,13 @@ import java.util.ArrayList;
  */
 public class FullScreenImageAdapter extends PagerAdapter {
     private Activity activity;
-    private ArrayList<String> imagePaths;
+    private ArrayList<Integer> imagePaths;
     private LayoutInflater inflater;
+    private View viewLayout;
+    private ViewGroup container;
+    private Integer pos;
 
-    public FullScreenImageAdapter(Activity activity, ArrayList<String> imagePaths) {
+    public FullScreenImageAdapter(Activity activity, ArrayList<Integer> imagePaths) {
         this.activity = activity;
         this.imagePaths = imagePaths;
     }
@@ -35,25 +45,59 @@ public class FullScreenImageAdapter extends PagerAdapter {
 
     @Override
     public boolean isViewFromObject(View view, Object object) {
-        return view == ((RelativeLayout) object);
+        return view == object;
     }
 
     @Override
-    public Object instantiateItem(ViewGroup container, int position) {
-        ImageView imgDisplay;
+    public Object instantiateItem(final ViewGroup container, final int position) {
+        this.container = container;
+        pos = position;
 
-        inflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View viewLayout = inflater.inflate(R.layout.layout_fullscreen_image, container, false);
-
-        imgDisplay = (ImageView) viewLayout.findViewById(R.id.imgDisplay);
-
-        BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inPreferredConfig = Bitmap.Config.ARGB_8888;
-        Bitmap bitmap = BitmapFactory.decodeFile(imagePaths.get(position), options);
-        imgDisplay.setImageBitmap(bitmap);
-
-        ((ViewPager) container).addView(viewLayout);
-
+        AsyncImageLoader loader = new AsyncImageLoader();
+        loader.execute(pos);
+/*
+        View result = null;
+        try {
+            result = loader.get();
+        } catch (Exception e) {
+        }
+*/
         return viewLayout;
+    }
+
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((View) object);
+    }
+
+    private class AsyncImageLoader extends AsyncTask<Integer, View, View> {
+        private Bitmap bitmap;
+        private Integer position;
+
+        @Override
+        protected View doInBackground(Integer... params) {
+            position = params[0];
+
+            InputStream raw = activity.getResources().openRawResource(imagePaths.get(position));
+            bitmap = BitmapFactory.decodeStream(raw);
+
+            return viewLayout;
+        }
+
+        @Override
+        protected void onPostExecute(View result) {
+            if(pos == position) {
+                ImageView imgDisplay = (ImageView) result.findViewById(R.id.imgDisplay);
+                imgDisplay.setImageBitmap(bitmap);
+                container.addView(result);
+            }
+        }
+
+        @Override
+        protected void onPreExecute() {
+            inflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            viewLayout = inflater.inflate(R.layout.layout_fullscreen_image, container, false);
+        }
     }
 }
--- app/src/main/java/keinerweiss/eventertainer/GalleryActivity.java
+++ app/src/main/java/keinerweiss/eventertainer/GalleryActivity.java
@@ -1,9 +1,13 @@
 package keinerweiss.eventertainer;
 
+import android.net.Uri;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 public class GalleryActivity extends AppCompatActivity {
 
@@ -14,5 +18,23 @@ public class GalleryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery);
+
+        ArrayList<Integer> images = new ArrayList<>();
+        images.add(R.raw.gallery1);
+        images.add(R.raw.gallery1);
+        images.add(R.raw.gallery3);
+        images.add(R.raw.gallery4);
+        images.add(R.raw.gallery5);
+        images.add(R.raw.gallery6);
+        images.add(R.raw.gallery7);
+        images.add(R.raw.gallery8);
+        images.add(R.raw.gallery9);
+        images.add(R.raw.gallery10);
+        images.add(R.raw.gallery11);
+
+
+        mPager = (ViewPager) findViewById(R.id.pager);
+        PagerAdapter mPagerAdapter = new FullScreenImageAdapter(GalleryActivity.this, images);
+        mPager.setAdapter(mPagerAdapter);
     }
 }
